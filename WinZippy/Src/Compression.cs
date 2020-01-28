9dc1229c4d88226db158dcc8d7f2cb7ceb8d846c
Dan Gillespie <daniel.gillespie@pluto.tv>
2020-01-27 22:39:25 -0800

    working on zip extraction

---
+++ WinZippy/Src/Compression.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace WinZippy
+{
+    public class Compression
+    {
+        public Compression()
+        {
+           
+            
+        }
+
+        public static void CompressFileOrFolder(string path)
+        {
+            var filename = path.Substring(path.LastIndexOf('\\') + 1);
+            if (File.Exists(path))
+            {
+                var pos = filename.LastIndexOf('.');
+                if (pos > -1)
+                {
+                    filename = filename.Substring(0, pos);
+                }   
+            }
+
+
+        }
+
+        private void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+        {
+            var files = Directory.GetFiles(path);
+
+            foreach (var filename in files)
+            {
+                var fi = new FileInfo(filename);
+
+                // Make the name in zip based on the folder
+                var entryName = filename.Substring(folderOffset);
+
+                // Remove drive from name and fixe slash direction
+                entryName = ZipEntry.CleanName(entryName);
+
+                var newEntry = new ZipEntry(entryName);
+
+                // Note the zip format stores 2 second granularity
+                newEntry.DateTime = fi.LastWriteTime;
+
+                // Specifying the AESKeySize triggers AES encryption. 
+                // Allowable values are 0 (off), 128 or 256.
+                // A password on the ZipOutputStream is required if using AES.
+                //   newEntry.AESKeySize = 256;
+
+                // To permit the zip to be unpacked by built-in extractor in WinXP and Server2003,
+                // WinZip 8, Java, and other older code, you need to do one of the following: 
+                // Specify UseZip64.Off, or set the Size.
+                // If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, 
+                // you do not need either, but the zip will be in Zip64 format which
+                // not all utilities can understand.
+                //   zipStream.UseZip64 = UseZip64.Off;
+                newEntry.Size = fi.Length;
+
+                zipStream.PutNextEntry(newEntry);
+
+                // Zip the file in buffered chunks
+                // the "using" will close the stream even if an exception occurs
+                var buffer = new byte[4096];
+                using (FileStream fsInput = File.OpenRead(filename))
+                {
+                    StreamUtils.Copy(fsInput, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+
+            // Recursively call CompressFolder on all folders in path
+            var folders = Directory.GetDirectories(path);
+            foreach (var folder in folders)
+            {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
+        }
+
+    }
+}
---
+++ WinZippy/Src/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinZippy
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length >= 3)
+            {
+                // expecting a command
+                // e.g. app.exe --compress C:\example
+                MessageBox.Show(String.Join(" ", args));
+                if (args[1] == "--compress")
+                {
+
+                }
+       
+                ShowUi();
+            }
+            else if (args.Length >= 2)
+            {
+                // expecting a file
+                // e.g. app.exe C:\example.zip
+                MessageBox.Show(String.Join(" ", args));
+                ShowUi();
+            }
+            else
+            {
+                // unknown command
+                MessageBox.Show("Unexpected Input.");
+            }
+        }
+
+        private static void ShowUi()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrameProgress());
+        }
+    }
+}
